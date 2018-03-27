a693fb6dfea8f0219bd4c006a95db858d4f9d50f
Maxime Baconnais <m.baconnais@outlook.com>
2018-03-27 15:51:13 -0700

    Add level 4 and bugfixes

---
+++ Assets/scripts/Music.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour {
+
+    public GameObject musicPlayer;
+
+    void Awake()
+    {
+        musicPlayer = GameObject.Find("MUSIC");
+        if (musicPlayer == null)
+        {
+            musicPlayer = this.gameObject;
+            musicPlayer.name = "MUSIC";
+            DontDestroyOnLoad(musicPlayer);
+        }
+        else
+        {
+            if (this.gameObject.name != "MUSIC")
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
--- Assets/scripts/PressAnyKey.cs
+++ Assets/scripts/PressAnyKey.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 
 public class PressAnyKey : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    bool lastStatus = false;
+
+    // Use this for initialization
+    void Start () {
+        this.lastStatus = Input.anyKeyDown;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.anyKeyDown())
+        if (Input.anyKeyDown)
         {
+            if (this.lastStatus)
+                return;
             if (SceneManager.GetActiveScene().buildIndex - 1 == SceneManager.sceneCount)
                 SceneManager.LoadScene(0);
             else
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
-
+        this.lastStatus = Input.anyKeyDown;
     }
 }
