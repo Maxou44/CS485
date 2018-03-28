d409826ffe211f6b9ab967c134f5deafbb27e792
Maxime Baconnais <m.baconnais@outlook.com>
2018-03-27 17:01:20 -0700

    Add level 5 & 6 + bugfxes

--- Assets/scripts/Music.cs
+++ Assets/scripts/Music.cs
@@ -4,23 +4,11 @@ using UnityEngine;
 
 public class Music : MonoBehaviour {
 
-    public GameObject musicPlayer;
-
     void Awake()
     {
-        musicPlayer = GameObject.Find("MUSIC");
-        if (musicPlayer == null)
-        {
-            musicPlayer = this.gameObject;
-            musicPlayer.name = "MUSIC";
-            DontDestroyOnLoad(musicPlayer);
-        }
-        else
-        {
-            if (this.gameObject.name != "MUSIC")
-            {
-                Destroy(this.gameObject);
-            }
-        }
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);
     }
 }
--- Assets/scripts/PressAnyKey.cs
+++ Assets/scripts/PressAnyKey.cs
@@ -5,24 +5,14 @@ using UnityEngine;
 
 public class PressAnyKey : MonoBehaviour {
 
-    bool lastStatus = false;
-
-    // Use this for initialization
-    void Start () {
-        this.lastStatus = Input.anyKeyDown;
-    }
-	
 	// Update is called once per frame
 	void Update () {
         if (Input.anyKeyDown)
         {
-            if (this.lastStatus)
-                return;
-            if (SceneManager.GetActiveScene().buildIndex - 1 == SceneManager.sceneCount)
+            if (SceneManager.GetActiveScene().buildIndex == 7)
                 SceneManager.LoadScene(0);
             else
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
-        this.lastStatus = Input.anyKeyDown;
     }
 }
